89f293abf02cc27814c86ba3da360b2657bf59f8
MetaMaxfield <hardmen.mail@gmail.com>
2024-08-15 12:58:07 +0400

    feature: добавление пакета с приложением blog
    Начальная интеграция приложения blog в проект, обеспечивающая необходимую базу для дальнейшего развития функциональности.
    
    - Добавлен пакет с приложением blog:
        - Пакет migrations с файлами миграции
        - Файл инициализации и определения пакета __init__.py
        - Файл admin.py с зарегистрированными моделями приложения в системе администрирования. В файле закомментировано поле author, связанное с моделью другого приложения, а также закомментированы переопределенные методы get_queryset(), get_fieldsets(), save_model()
        - Файл apps.py с конфигурацией приложения
        - Файл models.py с моделями данных приложения. В файле закомментировано поле author, связанное с моделью другого приложения
        - Файл tests.py для создания тестов для приложения
        - Файл urls.py с шаблонами адресов проекта
        - Файл views.py с логикой приложения

---
+++ blog/models.py
@@ -0,0 +1,139 @@
+from django.core.validators import FileExtensionValidator
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from taggit.managers import TaggableManager
+
+
+class Category(models.Model):
+    """Котегории"""
+
+    name = models.CharField(verbose_name='Категория', max_length=150)
+    description = models.TextField(verbose_name='Описание')
+    url = models.SlugField(max_length=160, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Video(models.Model):
+    """Видео"""
+
+    title = models.CharField(max_length=100, verbose_name='Заголовок видео')
+    description = models.TextField(verbose_name='Описание видео')
+    file = models.FileField(
+        upload_to='video/', validators=[FileExtensionValidator(allowed_extensions=['mp4'])], verbose_name='Видеофайл'
+    )
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('-create_at',)
+        verbose_name = 'Видеозапись'
+        verbose_name_plural = 'Видеозаписи'
+
+
+class Post(models.Model):
+    """Пост"""
+
+    title = models.CharField(verbose_name='Заголовок', max_length=250)
+    url = models.SlugField(max_length=25, unique_for_date='publish', unique=True)
+    # author = models.ForeignKey(
+    #     'users.CustomUser', verbose_name='Автор', on_delete=models.CASCADE, related_name='post_author', null=True
+    # )
+    category = models.ForeignKey(
+        'blog.Category', verbose_name='Категория', related_name='post_category', on_delete=models.SET_NULL, null=True
+    )
+    tags = TaggableManager(related_name='post_tags')
+    body = models.TextField(verbose_name='Содержание')
+    video = models.OneToOneField(
+        Video,
+        verbose_name='Видео к записи',
+        related_name='post_video',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(verbose_name='Изображение', upload_to='posts/')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    draft = models.BooleanField(default=False, verbose_name='Черновик')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.url})
+
+    def get_comment(self):
+        return self.comments.filter(parent__isnull=True, active=True).prefetch_related('parent_comments')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ('-publish',)
+
+
+class Comment(models.Model):
+    """Комментарии"""
+
+    post = models.ForeignKey('blog.Post', verbose_name='Запись', on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey(
+        'self',
+        verbose_name='Родитель',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='parent_comments',
+    )
+    name = models.CharField(verbose_name='Имя', max_length=80)
+    email = models.EmailField()
+    text = models.TextField(verbose_name='Содержание комментария', max_length=5000)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Комментарий от {self.name} к {self.post}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('created',)
+
+
+class Mark(models.Model):
+    """Оценка"""
+
+    nomination = models.CharField(verbose_name='Наименование', max_length=10)
+    value = models.SmallIntegerField(verbose_name="Значение", default=0)
+
+    def __str__(self):
+        return self.nomination
+
+    class Meta:
+        verbose_name = 'Значение рейтинга'
+        verbose_name_plural = 'Значения рейтинга'
+        ordering = ('value',)
+
+
+class Rating(models.Model):
+    """Рейтинг"""
+
+    ip = models.CharField(verbose_name='IP адрес', max_length=15)
+    mark = models.ForeignKey('blog.Mark', verbose_name='Оценка', on_delete=models.CASCADE)
+    post = models.ForeignKey('blog.Post', verbose_name='Пост', on_delete=models.CASCADE, related_name='rating_post')
+
+    def __str__(self):
+        return f'{self.mark}'
+
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
