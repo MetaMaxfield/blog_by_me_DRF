bef9ed0ce8a8415d8f0919f4e7ba192aea64f8c2
MetaMaxfield <hardmen.mail@gmail.com>
2024-08-22 06:46:15 +0400

    chore: добавление django-debug-toolbar
    Добавлен инструмент django-debug-toolbar для мониторинга запросов и улучшения отладки в процессе разработки.
    
    - В settings.py обновлены настройки MIDDLEWARE и INSTALLED_APPS для работы с debug_toolbar
    - В blog_by_me_DRF/urls.py настроен URL маршрут для подключения панели инструментов debug_toolbar в режиме отладки

--- blog_by_me_DRF/settings.py
+++ blog_by_me_DRF/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'debug_toolbar',
 ]
 
 
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 
