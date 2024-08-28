97bd5116049559c8f31cb9eb76019c0a68090be0
MetaMaxfield <hardmen.mail@gmail.com>
2024-08-28 06:46:32 +0400

    feature: добавление VideoDetailSerializer(serializers.ModelSerializer)
    Добавлен вложенный сериализатор VideoDetailSerializer(serializers.ModelSerializer), который необходим для удобной обработки и представления связанных данных в API.
    
    - Реализован сериализатор VideoDetailSerializer(serializers.ModelSerializer) для связанного объекта модели Video
    - Обновлен сериализатор PostDetailSerializer(TaggitSerializer, serializers.ModelSerializer). Добавлен вложенный сериализатор VideoDetailSerializer(serializers.ModelSerializer)

--- blog/serializers.py
+++ blog/serializers.py
@@ -15,10 +15,19 @@ class PostsSerializer(TaggitSerializer, serializers.ModelSerializer):
         exclude = ('created', 'updated', 'draft', 'video')
 
 
+class VideoDetailSerializer(serializers.ModelSerializer):
+    """Видеозапись"""
+
+    class Meta:
+        model = Video
+        fields = ('title', 'file')
+
+
 class PostDetailSerializer(TaggitSerializer, serializers.ModelSerializer):
     """Отдельный пост"""
 
     category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    video = VideoDetailSerializer(read_only=True)
     tags = TagListSerializerField()
     ncomments = serializers.IntegerField()
 
