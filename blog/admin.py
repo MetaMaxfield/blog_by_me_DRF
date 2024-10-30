8079df36fcc02f2966a14149b779850b24dedb48
MetaMaxfield <hardmen.mail@gmail.com>
2024-10-30 11:00:12 +0400

    feature: улучшение функционала администрирования для модели Post
    Эти изменения способствуют более строгой валидации данных, улучшают интерфейс для администраторов, обеспечивают корректное отображение данных для курсорной пагинации и добавляют возможность отложенной публикации постов.
    
    - В файле blog/admin.py добавлены улучшения для управления постами в административной панели:
        - Обновлена сортировка при выводе списка постов на основе полей draft и publish, что упрощает отображение постов
        - Поле publish добавлено в раздел 'Настройки' для предоставления возможности редактирования поля в административной панели
        - Добавлена валидация для поля publish в форме PostAdminForm, чтобы дата публикации не могла быть ранее сегодняшнего дня. При попытке установить недопустимую дату возникает ошибка валидации
        - Переопределен метод get_readonly_fields для добавления поля publish в список только для чтения, если объект существует и дата публикации уже наступила, чтобы предотвратить изменения даты публикации после ее наступления. Это позволяет соблюдать требования к курсорной пагинации, так как поле publish становится неизменным после публикации на сайте блога, и добавляет функционал отложенной публикации постов

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django import forms
 from django.contrib import admin
+from django.utils import timezone
 
 # from django.contrib.auth.models import Group
 from django.utils.safestring import mark_safe
@@ -12,9 +13,19 @@ admin.site.register(Comment)
 
 
 class PostAdminForm(forms.ModelForm):
-    """Ckeditor для поля "body" модели блога"""
+    """Настройки CKEditor для поля "body" и валидация даты публикации в модели Post"""
 
     body = forms.CharField(label='Содержание', widget=CKEditorUploadingWidget())
+
+    def clean_publish(self):
+        """
+        Проверяет, чтобы дата публикации была не раньше сегодняшнего дня
+        (время в пределах указанной даты может быть прошедшим)
+        """
+        publish_date = self.cleaned_data['publish']
+        if publish_date.date() < timezone.now().date():
+            raise forms.ValidationError("Дата публикации не может быть в прошлом.")
+        return publish_date
 
     class Meta:
         model = Post
@@ -102,7 +113,7 @@ class PostAdmin(admin.ModelAdmin):
     ]
     prepopulated_fields = {'url': ('title',)}
     date_hierarchy = 'publish'
-    ordering = ('draft', 'publish')
+    ordering = ('-draft', '-publish')
     save_on_top = True
     save_as = True
     inlines = [
@@ -134,7 +145,16 @@ class PostAdmin(admin.ModelAdmin):
                 )
             },
         ],
-        ['Настройки', {'fields': (('draft', 'url'),)}],
+        [
+            'Настройки',
+            {
+                'fields': (
+                    'draft',
+                    'url',
+                    'publish',
+                )
+            },
+        ],
     )
 
     def get_image(self, obj):
@@ -144,6 +164,16 @@ class PostAdmin(admin.ModelAdmin):
         return 'Нет изображения'
 
     get_image.short_description = 'Изображение'
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Добавление поля 'publish' в список только для чтения,
+        если объект существует и дата публикации уже наступила
+        """
+        returned_readonly_fields = self.readonly_fields.copy()
+        if obj and obj.publish <= timezone.now():
+            returned_readonly_fields.append('publish')
+        return returned_readonly_fields
 
     # def get_queryset(self, request):
     #     """
