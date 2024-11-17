7dd2160c46e497d0647be405af5a46093249d8f1
MetaMaxfield <hardmen.mail@gmail.com>
2024-11-17 17:50:14 +0400

    feature: расширение AuthorDetailSerializer
    Сериализатор AuthorDetailSerializer в приложении users ранее использовался в приложении blog для вложения в другие сериализаторы, где требовалась ограниченная информация о пользователе. Теперь он расширен для предоставления более детальной информации о пользователе, включая данные о группах, количестве постов и последних постах, что необходимо для случаев, когда требуется более полное отображение данных об авторе.
    
    - В файле users/serializers.py:
        - Добавлено поле get_user_groups, данные для которого поступают из метода get_user_groups модели User. Это необходимо для отображения групп пользователя
        - Добавлено поле nposts для отображения общего количества постов пользователя. Данные для этого поля будут поступать из аннотации в запросе к базе данных в представлении, которое будет добавлено в будущем
        - Добавлено поле last_posts и метод get_last_posts() для отображения последних трех постов пользователя. Поле last_posts получает данные через метод get_last_posts() сериализатора, который использует другой сериализатор PostsSerializer из приложения blog для формирования структуры данных. Импорт PostsSerializer произведен внутри метода для предотвращения циклического импорта
        - Изменен набор полей, исключены технические данные (например, пароль, права доступа) для повышения безопасности и минимизации избыточной информации

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from users.models import User
@@ -5,6 +6,10 @@ from users.models import User
 
 class AuthorDetailSerializer(serializers.ModelSerializer):
     """Данные автора"""
+
+    get_user_groups = serializers.ListField()
+    nposts = serializers.IntegerField()
+    last_posts = serializers.SerializerMethodField()
 
     def __init__(self, *args, **kwargs):
         fields = kwargs.pop('fields', None)
@@ -17,6 +22,17 @@ class AuthorDetailSerializer(serializers.ModelSerializer):
             for field_name in existing - allowed:
                 self.fields.pop(field_name)
 
+    def get_last_posts(self, obj):
+        from blog.serializers import PostsSerializer
+
+        return PostsSerializer(
+            obj.post_author.filter(draft=False, publish__lte=timezone.now())
+            .order_by('-publish', '-id')
+            .select_related('category')[:3],
+            many=True,
+            fields=('title', 'category', 'url', 'body', 'image', 'publish'),
+        ).data
+
     class Meta:
         model = User
-        fields = '__all__'
+        exclude = ('password', 'last_login', 'is_superuser', 'is_active', 'is_staff', 'groups', 'user_permissions')
