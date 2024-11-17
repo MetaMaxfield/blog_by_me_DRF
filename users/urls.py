913c108cad2666b8c0da45be90c80e8cd21788f9
MetaMaxfield <hardmen.mail@gmail.com>
2024-11-17 18:32:31 +0400

    feature: AuthorDetailView и маршрут URL
    Добавлен новый API функционал с использованием DRF, предоставляющий возможность работы с данными через REST API.
    
    - В файле users/views.py добавлен класс представления AuthorDetailView(APIView) для обработки GET-запросов с данными об авторе
    - В файле users/urls.py добавлен URL маршрут для класса представления AuthorDetailView(APIView)

--- users/urls.py
+++ users/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+
+from users import views
+
+urlpatterns = [
+    path('<int:pk>/', views.AuthorDetailView.as_view()),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.db.models import Count, Q
+from django.utils import timezone
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.models import User
+from users.serializers import AuthorDetailSerializer
+
+
+class AuthorDetailView(APIView):
+    """Вывод данных об авторе"""
+
+    def get(self, request, pk):
+        author = get_object_or_404(
+            User.objects.annotate(
+                nposts=Count(
+                    'post_author', filter=Q(post_author__draft=False, post_author__publish__lte=timezone.now())
+                )
+            ).defer('password', 'last_login', 'is_active', 'is_staff'),
+            id=pk,
+        )
+        author_serializer = AuthorDetailSerializer(author)
+        return Response(author_serializer.data)
