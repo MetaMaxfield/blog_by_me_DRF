790d86cfa5532d0980c1d766629e4fadc40d9daf
MetaMaxfield <hardmen.mail@gmail.com>
2025-01-12 20:40:59 +0400

    refactor: AboutView(RetrieveAPIView)
    В файле с представлениями приложения company класс AboutView переведён с наследования от APIView на RetrieveAPIView для упрощения и улучшения поддерживаемости кода.
    
    - В company/views.py:
        - Добавлен класс AboutView(generics.RetrieveAPIView)
        - Класс AboutView(APIView) закомментирован

--- company/views.py
+++ company/views.py
@@ -1,5 +1,5 @@
 from django.utils.translation import gettext as _
-from rest_framework import status
+from rest_framework import generics, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,14 +9,22 @@ from company.serializers import AboutSerializer, ContactSerializer
 from services.caching import get_cached_objects_or_queryset
 from services.company.send_mail import send
 
+# class AboutView(APIView):
+#     """Вывод информации о компании"""
+#
+#     def get(self, request: Request) -> Response:
+#         about = get_cached_objects_or_queryset(KEY_ABOUT)
+#         serializer = AboutSerializer(about)
+#         return Response(serializer.data)
 
-class AboutView(APIView):
+
+class AboutView(generics.RetrieveAPIView):
     """Вывод информации о компании"""
 
-    def get(self, request: Request) -> Response:
-        about = get_cached_objects_or_queryset(KEY_ABOUT)
-        serializer = AboutSerializer(about)
-        return Response(serializer.data)
+    serializer_class = AboutSerializer
+
+    def get_object(self):
+        return get_cached_objects_or_queryset(KEY_ABOUT)
 
 
 class ContactView(APIView):
