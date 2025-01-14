cf5883af3ce563508404b9a321f5bd95262a4924
MetaMaxfield <hardmen.mail@gmail.com>
2025-01-14 19:23:31 +0400

    refactor: ContactView(CreateAPIView)
    В файле с представлениями приложения company класс ContactView переведён с наследования от APIView на CreateAPIView для упрощения и улучшения поддерживаемости кода.
    
    - В company/views.py:
        - Добавлен класс ContactView(generics.CreateAPIView)
        - Класс ContactView(APIView) закомментирован

--- company/views.py
+++ company/views.py
@@ -1,13 +1,16 @@
 from django.utils.translation import gettext as _
 from rest_framework import generics, status
-from rest_framework.request import Request
+
+# from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from blog_by_me_DRF.settings import KEY_ABOUT
 from company.serializers import AboutSerializer, ContactSerializer
 from services.caching import get_cached_objects_or_queryset
 from services.company.send_mail import send
+
+# from rest_framework.views import APIView
+
 
 # class AboutView(APIView):
 #     """Вывод информации о компании"""
@@ -27,15 +30,31 @@ class AboutView(generics.RetrieveAPIView):
         return get_cached_objects_or_queryset(KEY_ABOUT)
 
 
-class ContactView(APIView):
+# class ContactView(APIView):
+#     """Добавление сообщения обратной связи"""
+#
+#     def post(self, request: Request) -> Response:
+#         serializer = ContactSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             send(serializer.validated_data['email'])
+#             return Response(
+#                 {'message': _('Сообщение успешно отправлено администрации проекта.')}, status=status.HTTP_201_CREATED
+#             )
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ContactView(generics.CreateAPIView):
     """Добавление сообщения обратной связи"""
 
-    def post(self, request: Request) -> Response:
-        serializer = ContactSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            send(serializer.validated_data['email'])
-            return Response(
-                {'message': _('Сообщение успешно отправлено администрации проекта.')}, status=status.HTTP_201_CREATED
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = ContactSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+        send(serializer.validated_data['email'])
+
+    def create(self, request, *args, **kwargs):
+        super().create(request, *args, **kwargs)
+        return Response(
+            {'message': _('Сообщение успешно отправлено администрации проекта.')}, status=status.HTTP_201_CREATED
+        )
