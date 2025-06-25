273d4fb74ab03851bba00460e3dec60810298f07
MetaMaxfield <hardmen.mail@gmail.com>
2025-06-25 18:30:13 +0400

    test: добавление VideoFactory
    Добавлена фабрика для упрощённого создания тестовых данных для модели видеозаписей.
    
    - В blog/factories.py пакета с тестами добавлена фабрика VideoFactory(DjangoModelFactory) для модели Video

--- tests/blog/factories.py
+++ tests/blog/factories.py
@@ -1,7 +1,8 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from factory import Sequence
 from factory.django import DjangoModelFactory
 
-from blog.models import Category
+from blog.models import Category, Video
 
 
 class CategoryFactory(DjangoModelFactory):
@@ -11,3 +12,12 @@ class CategoryFactory(DjangoModelFactory):
     name = Sequence(lambda n: f'Категория {n+1}')
     description = 'Описание категории'
     url = Sequence(lambda n: f'category{n+1}')
+
+
+class VideoFactory(DjangoModelFactory):
+    class Meta:
+        model = Video
+
+    title = 'Рыбалка'
+    description = 'Видео с большим уловом'
+    file = SimpleUploadedFile("fishing.mp4", b"fake video content", content_type="video/mp4")
