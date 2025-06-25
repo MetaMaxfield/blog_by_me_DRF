43e0b16e1b949341816e057d93836824950dbb8d
MetaMaxfield <hardmen.mail@gmail.com>
2025-06-25 18:41:13 +0400

    test: модель Video
    Добавлены тесты для модели Video с проверкой логики, определённой в коде проекта.
    
    - В blog/test_models.py пакета с тестами добавлен класс VideoModelTest, содержащий:
        - test_title_max_lenght() для проверки максимальной длины поля 'title'
        - test_title_verbose_name() для проверки метки поля 'title'
        - test_description_verbose_name() для проверки метки поля 'description'
        - test_file_upload_to() для проверки пути загрузки файлов для поля 'file'
        - test_file_validators() для проверки валидаторов, применяемых к полю 'file'
        - test_file_verbose_name() для проверки метки поля 'file'
        - test_create_at_auto_now_add() для проверки автоматического добавления времени создания в поле 'create_at'
        - test_object_name_is_title(video) c тестовой базой данных и фикстурой для проверки метода __str__() на основе поля 'title'
        - test_model_ordering() для проверки порядка сортировки объектов модели
        - test_model_verbose_name() для проверки 'verbose_name' модели
        - test_model_verbose_name_plural() для проверки 'verbose_name_plural' модели

--- tests/blog/test_models.py
+++ tests/blog/test_models.py
@@ -1,6 +1,7 @@
 import pytest
+from django.core.validators import FileExtensionValidator
 
-from blog.models import Category
+from blog.models import Category, Video
 
 
 class CategoryModelTest:
@@ -45,4 +46,64 @@ class CategoryModelTest:
     def test_model_verbose_name_plural(self):
         fact_verbose_name_plural = Category._meta.verbose_name_plural
         expected_verbose_name_plural = 'Категории'
+        assert fact_verbose_name_plural == expected_verbose_name_plural
+
+
+class VideoModelTest:
+    """Тестирование модели Video"""
+
+    def test_title_max_length(self):
+        fact_max_length = Video._meta.get_field('title').max_length
+        expected_max_length = 100
+        assert fact_max_length == expected_max_length
+
+    def test_title_verbose_name(self):
+        fact_verbose_name = Video._meta.get_field('title').verbose_name
+        expected_verbose_name = 'Заголовок видео'
+        assert fact_verbose_name == expected_verbose_name
+
+    def test_description_verbose_name(self):
+        fact_verbose_name = Video._meta.get_field('description').verbose_name
+        expected_verbose_name = 'Описание видео'
+        assert fact_verbose_name == expected_verbose_name
+
+    def test_file_upload_to(self):
+        fact_upload_to = Video._meta.get_field('file').upload_to
+        expected_upload_to = 'video/'
+        assert fact_upload_to == expected_upload_to
+
+    def test_file_validators(self):
+        fact_validators = Video._meta.get_field('file').validators
+        expected_validators = [FileExtensionValidator(allowed_extensions=['mp4'])]
+        assert fact_validators == expected_validators
+
+    def test_file_verbose_name(self):
+        fact_verbose_name = Video._meta.get_field('file').verbose_name
+        expected_verbose_name = 'Видеофайл'
+        assert fact_verbose_name == expected_verbose_name
+
+    def test_create_at_auto_now_add(self):
+        fact_auto_now_add = Video._meta.get_field('create_at').auto_now_add
+        expected_auto_now_add = True
+        assert fact_auto_now_add is expected_auto_now_add
+
+    @pytest.mark.django_db
+    def test_object_name_is_title(self, video):
+        fact_object_name = str(video)
+        expected_object_name = video.title
+        assert fact_object_name == expected_object_name
+
+    def test_model_ordering(self):
+        fact_ordering = Video._meta.ordering
+        expected_ordering = ('-create_at',)
+        assert fact_ordering == expected_ordering
+
+    def test_model_verbose_name(self):
+        fact_verbose_name = Video._meta.verbose_name
+        expected_verbose_name = 'Видеозапись'
+        assert fact_verbose_name == expected_verbose_name
+
+    def test_model_verbose_name_plural(self):
+        fact_verbose_name_plural = Video._meta.verbose_name_plural
+        expected_verbose_name_plural = 'Видеозаписи'
         assert fact_verbose_name_plural == expected_verbose_name_plural
