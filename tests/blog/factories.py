2f7f8d7b1c6bd0bd81f939bab869f6e485556fe3
MetaMaxfield <hardmen.mail@gmail.com>
2025-06-25 22:30:35 +0400

    test: добавление PostFactory
    Добавлена фабрика для упрощённого создания тестовых данных для модели постов.
    
    - В blog/factories.py пакета с тестами добавлена фабрика PostFactory(DjangoModelFactory) для модели Post

--- tests/blog/factories.py
+++ tests/blog/factories.py
@@ -1,8 +1,9 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
-from factory import Sequence
+from factory import Sequence, SubFactory
 from factory.django import DjangoModelFactory
 
-from blog.models import Category, Video
+from blog.models import Category, Post, Video
+from tests.users.factories import UserFactory
 
 
 class CategoryFactory(DjangoModelFactory):
@@ -21,3 +22,15 @@ class VideoFactory(DjangoModelFactory):
     title = 'Рыбалка'
     description = 'Видео с большим уловом'
     file = SimpleUploadedFile("fishing.mp4", b"fake video content", content_type="video/mp4")
+
+
+class PostFactory(DjangoModelFactory):
+    class Meta:
+        model = Post
+
+    title = Sequence(lambda n: f'Как я провёл отпуск. Часть {n+1}')
+    url = Sequence(lambda n: f'kak-ya-provel-otpusk{n+1}')
+    author = SubFactory(UserFactory)
+    category = SubFactory(CategoryFactory)
+    body = 'Содержание событий в отпуске'
+    image = SimpleUploadedFile("fishing.jpeg", b"fake image content", content_type="image/jpeg")
