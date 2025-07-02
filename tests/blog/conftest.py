9ef73575a15f84df9350ae155f340c4d30b2fbad
MetaMaxfield <hardmen.mail@gmail.com>
2025-07-02 18:51:54 +0400

    test: фикстура filtered_posts_and_timezone_now
    В файл локальной конфигурации тестов приложения blog добавлена фикстура для создания набора постов и фиксации текущего времени с целью стабильной проверки логики выборки.
    
    - В blog/conftest.py пакета с тестами добавлена фикстура filtered_posts_and_timezone_now()

--- tests/blog/conftest.py
+++ tests/blog/conftest.py
@@ -1,6 +1,14 @@
-import pytest
+from datetime import timedelta
+from unittest import mock
 
+import pytest
+from django.core.management import call_command
+from django.utils import timezone
+
+from blog.models import Mark
+from services import queryset
 from tests.blog.factories import CategoryFactory, PostFactory, VideoFactory
+from tests.users.factories import UserFactory
 
 
 @pytest.fixture
@@ -16,3 +24,25 @@ def video():
 @pytest.fixture
 def post():
     return PostFactory()
+
+
+@pytest.fixture
+def filtered_posts_and_timezone_now():
+    time_now = timezone.now()
+
+    user = UserFactory()
+    category = CategoryFactory()
+
+    # Создание двух ожидаемых постов
+    PostFactory.create_batch(2, author=user, category=category, publish=time_now, draft=False)
+
+    # Создание постов не проходящих фильтрацию
+    PostFactory.create(author=user, category=category, publish=time_now, draft=True)  # Пост-черновик
+    PostFactory.create(author=user, category=category, publish=time_now + timedelta(seconds=30))  # Неопубликованный
+
+    # Мок для использования единого времени в запросах фикстуры и в запросах тестов (publish__lte=timezone.now())
+    with mock.patch('services.queryset.timezone.now') as mock_timezone:
+        mock_timezone.return_value = time_now
+        filtered_posts = queryset._qs_simple_post_list()
+
+    return {'filtered_posts': filtered_posts, 'time_now': time_now}
